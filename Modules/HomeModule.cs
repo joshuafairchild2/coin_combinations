2d97463641715de76a4f8a5cf428c77e45017b8d
Anabel Ramirez and Joshua Fairchild <anabel+joshua@>
2017-05-30 15:20:50 -0700

    add module routing and cshtml pages for a running web app.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,4 +1,6 @@
 using Nancy;
+using System;
+using System.Collections.Generic;
 
 namespace CoinCombinations
 {
@@ -6,7 +8,14 @@ namespace CoinCombinations
   {
     public HomeModule()
     {
-      Get["/"] = _ => "hello world";
+      Get["/"] = _ => View["index.cshtml"];
+      Get["/sorted"] = _ => {
+        CombinationGenerator myGenerator = new CombinationGenerator();
+        string userInput = Request.Query["userInput"];
+        decimal inputAsDecimal = Decimal.Parse(userInput);
+        List<string> coins = myGenerator.Generate(inputAsDecimal);
+        return View["result.cshtml", coins];
+      };
     }
   }
 }
